7e7be87e1fd926865db29ced02b5f580ec1f463d
Kazuke23 <sebastian.buitrago01@unicatolica.edu.co>
2024-10-26 15:20:59 -0500

    Configuracion de ruta con vercel

--- src/app.js
+++ src/app.js
@@ -10,15 +10,19 @@ const codigoRoutes = require('./routes/codigo.routes');
 const intentoRoutes = require('./routes/intento.routes');
 const ganadoresRoutes = require('./routes/ganadores.routes');
 
+// Configurar CORS para permitir solicitudes desde tu frontend
+app.use(cors({
+  origin: 'https://front-juego.vercel.app', // dominio de tu frontend
+}));
 
-app.use(cors());
 app.use(express.json()); // Para poder parsear JSON
 
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('Conectado a MongoDB'))
+})
+  .then(() => console.log('Conectado a MongoDB'))
   .catch((error) => console.error('Error al conectar a MongoDB:', error));
 
 // Usar las rutas de autenticación
@@ -27,7 +31,6 @@ app.use('/api/user', userRoutes); // Cambié la ruta base de userRoutes
 app.use('/api/codigo', codigoRoutes);
 app.use('/api/intento', intentoRoutes);
 app.use('/api/ganadores', ganadoresRoutes);
-
 
 // Middleware para manejar errores
 app.use((err, req, res, next) => {
